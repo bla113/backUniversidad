9d3deb351fb190c2d3a132b28261ac100bf8f8b0
bla113 <32277733+bla113@users.noreply.github.com>
2023-05-02 13:17:03 -0600

    Update Conexion.php
    Update Conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,7 +6,7 @@ class Conexion{
 
 		$link = new PDO("mysql:host=localhost;dbname=ucem_v1",
 			            "root",
-			            "");
+			            "Amh8e8mr001");
 
 		$link->exec("set names utf8");
 
